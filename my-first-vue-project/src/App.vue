025f2b167cb81d5dc5751f9eb790b825878a16f5
kk <517412663@qq.com>
2017-05-10 23:07:01 +0800

    todolistFinished

--- my-first-vue-project/src/App.vue
+++ my-first-vue-project/src/App.vue
@@ -3,11 +3,13 @@
     <h1>{{title}}</h1>
     <h1 v-text="title"></h1>
     <h1 v-html="title"></h1>
+    <input type="text" v-model="newItem" v-on:keyup.enter="addNew">
     <ul>
-        <li v-for="item in items" v-bind:class="{finished:item.isFinished}">
+        <li v-for="item in items" v-bind:class="{finished:item.isFinished}" v-on:click="toggleFinish(item)">
         {{item.label}}
         </li>
     </ul>
+
   </div>
 </template>
 
@@ -29,7 +31,23 @@ export default {
             isFinished:true
   
           }
-      ]
+      ],
+      newItem:''
+    }
+  },
+  methods:{
+    toggleFinish:function(item){
+      // console.log(item);
+      item.isFinished=!item.isFinished;
+    },
+    addNew:function(){
+      // this.newItem;
+      // console.log(this.newItem);
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      })
+      this.newItem='';
     }
   }
 }
