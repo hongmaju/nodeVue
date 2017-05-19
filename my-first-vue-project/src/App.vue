491cae2cce5211ab12621a43d2b9fdf725aad479
HongMaJu <517412663@qq.com>
2017-05-19 10:33:59 +0800

    vue中如何动态的绑定图片，vue中通过data返回图片路径

--- my-first-vue-project/src/App.vue
+++ my-first-vue-project/src/App.vue
@@ -1,53 +1,38 @@
 <template>
   <div id="App">
-    <h1>{{title}}</h1>
-   <!-- 工具栏 -->
-            <nav class="bar bar-tab">
-                <a class="tab-item external active" href="#">
-                    <span class="icon icon-home"></span>
-                    <span class="tab-label">首页</span>
-                </a>
-                <a class="tab-item external" href="#">
-                    <span class="icon icon-star"></span>
-                    <span class="tab-label">收藏</span>
-                </a>
-                <a class="tab-item external" href="#">
-                    <span class="icon icon-settings"></span>
-                    <span class="tab-label">设置</span>
-                </a>
-    </nav>
+   
+<!--top轮播图片-->
+<Swiper></Swiper>
 
   </div>
 </template>
 
 <script>
-
- // import './js/jquery.js'
- // import './js/light7.js'
+import Swiper from './components/Swiper.vue'
 export default {
   
    data () {
     return {
-      title: '测试入口'
+     
       
     }
+
   },
   mounted:function () {
         //类似于jquery中的ready方法
         this.$nextTick(function () {
-    //        $.config = {
-    // autoInit: true //no recommend
-  // }
+    
         })
 
     },
   methods:{
-   }
+   },
+    components:{Swiper}
 }
 </script>
 <style>
   /*@import "./css/light7.css";*/
-  @import "./css/indexTest.css";
+ /* @import "./css/indexTest.css";*/
 </style>
 
 
---
+++ my-first-vue-project/src/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+ 
+<!--top轮播图片-->
+<div>
+   <img id="moveImgs" :src="now" alt="">
+   <img src="../Images/1.jpg" alt="">
+</div>
+
+</template>
+
+<script>
+
+export default {
+  // import Store from './js/axios.min.js'
+ 
+
+    data () {
+    return {
+    	pic:["./src/Images/1.jpg","./src/Images/2.jpg","./src/Images/3.jpg"],
+      now:require("../Images/1.jpg")
+      
+    }
+
+  },
+  mounted:function () {
+        //类似于jquery中的ready方法
+        this.$nextTick(function () {
+    		//this.getPic();
+        })
+
+    },
+  methods:{
+  	getPic:function(){
+
+				
+			var i=0;
+				//alert(this.pic.data[0].url)
+ 				 var _this=this;
+	 			setInterval(function(){
+
+			        if(i>=_this.pic.length){
+			        i=0;
+		       		}
+		       		 _this.now=_this.pic[i];
+		       		 var element = document.getElementById('moveImgs');
+				     element.src =  _this.now;
+		       		console.log(_this.now);
+		       		 i++;
+		     },2000);
+			
+		}
+   }
+  
+}
+</script>
+<style>
+
+</style>
